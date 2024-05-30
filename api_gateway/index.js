492b616f9759a4c9baa2109c00012604187b56f9
Felipe Escalona <escalonaf12@gmail.com>
2024-05-29 23:17:34 -0400

    fix(subjects): package.json start module

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -1,42 +1,55 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import dotenv from 'dotenv';
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-const userEndpoints = process.env.USER || 'http://localhost:3001/users';
-const courseEndpoints = process.env.COURSE || 'http://localhost:3002/courses';
+const userEndpoints = process.env.USER || "http://localhost:3001/users";
+const courseEndpoints = process.env.COURSE || "http://localhost:3002/courses";
+const subjectsEndpoints =
+  process.env.SUBJECT || "http://localhost:3003/subjects";
 console.log(userEndpoints);
 console.log(courseEndpoints);
 app.use(
-    '/users',
-    createProxyMiddleware({
-        target: userEndpoints,
-        changeOrigin: true,
-        pathRewrite: {
-            '^/users': '',
-        },
-    })
+  "/users",
+  createProxyMiddleware({
+    target: userEndpoints,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/users": "",
+    },
+  })
 );
 
 app.use(
-    '/courses',
-    createProxyMiddleware({
-        target: courseEndpoints,
-        changeOrigin: true,
-        pathRewrite: {
-            '^/courses': '',
-        },
-    })
+  "/courses",
+  createProxyMiddleware({
+    target: courseEndpoints,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/courses": "",
+    },
+  })
 );
 
-app.get('/', (req, res) => {
-    res.send('API Gateway');
+app.use(
+  "/subjects",
+  createProxyMiddleware({
+    target: courseEndpoints,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/subjects": "",
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("API Gateway");
 });
 
-const PORT = process.env.PORT|| 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`API Gateway running on port ${PORT}`);
+  console.log(`API Gateway running on port ${PORT}`);
 });
