54bff873f0a33f18ac72812d40854d2b20ed6382
Felipe Escalona <escalonaf12@gmail.com>
2024-06-02 21:53:07 -0400

    feature(users): adding gender to database

--- users/libs/validate.js
+++ users/libs/validate.js
@@ -1,15 +1,16 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 const userSchema = Joi.object({
-    _id: Joi.string().required(),
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    lastName: Joi.string().required(),
-    userType: Joi.string().valid('student', 'tutor').required(),
-    courses_student: Joi.array().items(Joi.string()),
-    courses_tutor: Joi.array().items(Joi.string()),
-    rating: Joi.number().min(0).max(5).required(),
-    ratingCount: Joi.number().min(0).required(),
+  _id: Joi.string().required(),
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  lastName: Joi.string().required(),
+  userType: Joi.string().valid("student", "tutor").required(),
+  courses_student: Joi.array().items(Joi.string()),
+  courses_tutor: Joi.array().items(Joi.string()),
+  rating: Joi.number().min(0).max(5).required(),
+  ratingCount: Joi.number().min(0).required(),
+  gender: Joi.string().valid("M", "F").required(),
 });
 
 export default userSchema;
--- users/models/User.js
+++ users/models/User.js
@@ -1,17 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: String,
-    name: String,
-    email: String,
-    lastName: String,
-    userType: String,
-    courses_student: [String],
-    courses_tutor: [String],
-    rating: { type: Number, default: 0 },
-    ratingCount: { type: Number, default: 0 },
+  _id: String,
+  name: String,
+  email: String,
+  gender: String,
+  lastName: String,
+  userType: String,
+  courses_student: [String],
+  courses_tutor: [String],
+  rating: { type: Number, default: 0 },
+  ratingCount: { type: Number, default: 0 },
 });
 
-const User = mongoose.model('usuario', userSchema);
+const User = mongoose.model("usuario", userSchema);
 
 export default User;
